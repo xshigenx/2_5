cde5066f4c5057933ca3c8bf571fba9c180b9f86
xshigenx <161622150+xshigenx@users.noreply.github.com>
2024-03-13 16:41:14 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+  public static void main(String[] args) {
+   int i=0;
+   int b=0;
+    while(i<301)
+      {
+      b=(9/5)*i+32;
+      System.out.println(i+ "  " +b);
+      i+=20;
+      }
+  }
+}
+ 
